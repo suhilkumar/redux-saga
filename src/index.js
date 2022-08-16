8a6aecdcdcdd758e5a3533799c7503ac793d833a
Suhilkumar <suhilkumarpatel@gmail.com>
2022-08-15 21:43:11 -0500

    create store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
+import store from './redux/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(store)
+console.log(store.getState())
+
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import {createStore} from 'redux'
+
+const rootReducer = () => 1;
+const store = createStore(rootReducer)
+
+export default store;
